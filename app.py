953bcb7d3a5ce264b16ec123c9767b3861404f61
ratatouille99 <115515737+ratatouille99@users.noreply.github.com>
2024-06-17 02:23:28 -0500

    swagger docs

--- app.py
+++ app.py
@@ -3,82 +3,24 @@ import pickle
 from sklearn.feature_extraction.text import CountVectorizer
 from tensorflow.keras.models import load_model
 import numpy as np
-from flasgger import Swagger
 
 app = Flask(__name__)
-swagger = Swagger(app)
 
-# Cargar el vectorizador guardado
+# Cargar el vectorizador y el modelo
 def load_vectorizer():
     with open('vectorizer.pkl', 'rb') as f:
         return pickle.load(f)
 
 vectorizer = load_vectorizer()
 
-# Cargar el modelo entrenado (suponiendo que sea un modelo de TensorFlow Keras)
 def load_trained_model():
     return load_model('sql_injection_model2.h5')
 
 model = load_trained_model()
 
-@app.route('/')
-def index():
-    """
-    Bienvenido a la API de predicción de SQL Injection.
-    ---
-    responses:
-      200:
-        description: Página de inicio de la API
-    """
-    return 'Bienvenido a la API de predicción de SQL Injection. Utilice el endpoint /prediccion para obtener predicciones.'
-
+# Ruta para la predicción de SQL injection
 @app.route('/prediccion', methods=['POST'])
 def prediccion():
-    """
-    Endpoint para obtener una predicción de SQL Injection.
-    ---
-    parameters:
-      - name: body
-        in: body
-        required: true
-        schema:
-          type: object
-          required:
-            - query
-          properties:
-            query:
-              type: string
-              description: La consulta SQL para analizar
-              example: "SELECT * FROM users WHERE id = 1"
-    responses:
-      200:
-        description: Resultado de la predicción
-        schema:
-          type: object
-          properties:
-            prediccion:
-              type: integer
-              description: La predicción, 0 o 1
-              example: 1
-      400:
-        description: Error en la solicitud
-        schema:
-          type: object
-          properties:
-            error:
-              type: string
-              description: Mensaje de error
-              example: "No se encontró la consulta en los datos JSON"
-      500:
-        description: Error interno del servidor
-        schema:
-          type: object
-          properties:
-            error:
-              type: string
-              description: Mensaje de error
-              example: "Error al realizar la predicción"
-    """
     data = request.get_json()
 
     if 'query' not in data:
@@ -96,10 +38,8 @@ def prediccion():
 
         prediction = model.predict(query_transformed)
 
-        # Redondear y convertir la predicción a entero (0 o 1)
         prediction_label = int(round(prediction[0][0]))
 
-        # Preparar la respuesta JSON
         response = {"prediccion": prediction_label} 
         
         return jsonify(response), 200
